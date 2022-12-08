fdb76493ee0445de7019759c64e86e2570b3dd4c
Tulugudina <gdnkms2013@gmail.com>
2022-12-08 10:16:38 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - main block
- * return: 0 (success)
+ * Return: o
  */
 int main(void)
 {
-	puts("\"programming is like develop a multilingual puzzle"); 
-	return (0);
+	puts("\"programming is like building a multilingual puzzle");
+        return (0);
 }
