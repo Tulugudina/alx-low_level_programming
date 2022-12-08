6e291a65d93cfef8fa3931f8025f962c51b51935
Tulugudina <gdnkms2013@gmail.com>
2022-12-08 11:00:19 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,23 @@
+#!/bin/bash
 #include <stdio.h>
+
+
+
 /**
- * main - main block
- * Return: o
- */
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
 int main(void)
+
 {
-	puts("\"programming is like building a multilingual puzzle");
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle"
+
+	    return (0);
+
 }
